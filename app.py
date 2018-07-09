0df68c88a99ecedc64db1bf3eb7d02dc6bb8ea63
twomonkeysth <40711687+twomonkeysth@users.noreply.github.com>
2018-07-09 19:56:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,17 @@ handler = WebhookHandler(channel_secret)
 
 @app.route('/')
 def homepage():
-
-    return "OK"
+    return 'OK'
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
